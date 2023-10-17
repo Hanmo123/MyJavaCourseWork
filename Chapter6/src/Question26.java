ecfb8c63cd478f6ac3a9abcd84dab860f49d88c1
Hanmo5888 <letsgo@88.com>
2023-10-17 09:09:22 +0800

    feat: 新增回文数判断

--- Chapter6/src/Question26.java
+++ Chapter6/src/Question26.java
@@ -1,16 +1,18 @@
-public class Question26 {
+public class Question26 extends Question3 {
     public static void main(String[] args) {
         int count = 0;
         int number = 1;
 
-        while (count != 100)
-            if (isPrime(++number)) {
+        while (count != 100) {
+            number++;
+            if (isPrime(number) && isPalindrome(number)) {
                 System.out.print(number + " ");
 
                 if (++count % 10 == 0) {
                     System.out.println();
                 }
             }
+        }
     }
 
     public static boolean isPrime(int number) {
